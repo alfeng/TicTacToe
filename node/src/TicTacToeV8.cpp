4407955855b4e0ecca7290aacec218bc65737905
Anthony <alf_eng@hotmail.com>
2018-05-02 15:06:31 -0700

    Outlined implementation

--- node/src/TicTacToeV8.cpp
+++ node/src/TicTacToeV8.cpp
@@ -68,28 +68,9 @@ NAN_METHOD(Startup)
 	int row = -1, col = -1;
 
 
-
 	// Set Javascript status delegate
 	// assume info[0]->IsFunction()
 	jsStatusDelegate.Reset(info[0].As<v8::Function>());
-
-	// 'info' is a macro's "implicit" parameter - it's a bridge object between C++ and JavaScript runtimes
-	// You would use info to both extract the parameters passed to a function as well as set the return value.
-	info.GetReturnValue().Set(message);
-}
-
-// Update CGDV on Main UI thread
-NAN_METHOD(NewGame)
-{
-	// Start CGDV
-	tttGame.Update();
-}
-
-// Revalidate CGDV on Main UI thread
-NAN_METHOD(Validate)
-{
-	// Start CGDV
-	tttGame->Revalidate();
 }
 
 // Shutdown CGDV on Main UI thread
@@ -99,22 +80,56 @@ NAN_METHOD(Shutdown)
 	tttGame->Shutdown();
 }
 
+// Reset game
+NAN_METHOD(NewGame)
+{
+	// Reset game
+	tttGame.NewGame();
+
+	// Reset local state
+}
+
+// Do player turn
+NAN_METHOD(DoTurn)
+{
+	// Extract Javascript parameters (row, column)
+
+	// Set player marker on specified ceel
+	tttGame->SetMark(row, col, X_MARKER);
+}
+
+// Do AI turn
+NAN_METHOD(DoAiTurn)
+{
+	// Make AI move
+	tttGame->SetMark(row, col, O_MARKER);
+
+	// Set parameters to Javascript (row, col)
+}
+
+// Check if game is over
+NAN_METHOD(IsGameOver)
+{
+	// Make AI move
+	bool gameOver = tttGame.IsGameOver(winRows, winCols);
+	if (gameOver)
+	{
+		// Determine type of end game
+	}
+
+	// Set parameters to Javascript (gameOver, winRows, winCols)
+}
+
 // Module initialization logic
 NAN_MODULE_INIT(Initialize)
 {
 	// Export CGDV methods
 	NAN_EXPORT(target, Startup);
-	NAN_EXPORT(target, Update);
-	NAN_EXPORT(target, Validate);
 	NAN_EXPORT(target, Shutdown);
-	NAN_EXPORT(target, Suspend);
-	NAN_EXPORT(target, Resume);
-
-	// Create CGDV App Helper
-	cgdvApp = CgdvApp::GetSingleton();
-
-	// Init CGDV status class so Javascript can use it
-	v8CgdvStatus::Init(target);
+	NAN_EXPORT(target, NewGame);
+	NAN_EXPORT(target, DoTurn);
+	NAN_EXPORT(target, DoAiTurn);
+	NAN_EXPORT(target, IsGameOver);
 }
 
 // Create the add-on module and initialize it (created with NAN_MODULE_INIT macro)
